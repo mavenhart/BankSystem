3ef96b5dc630c588b501a61e185eb9be0a5e81c3
mavenhart <42216061+mavenhart@users.noreply.github.com>
2018-08-09 03:53:30 +0800

    Add files via upload

---
+++ BankSystem.sql
@@ -0,0 +1,27 @@
+USE master
+
+CREATE DATABASE [BankSystem]
+GO
+
+USE [BankSystem]
+GO
+
+/****** Object:  Table [dbo].[Account]    Script Date: 8/9/2018 3:32:04 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Account](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[LoginName] [varchar](50) NOT NULL,
+	[AccountNumber] [varchar](50) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+	[Balance] [money] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+
